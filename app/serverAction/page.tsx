28065a5086d1869e13d885782c9a463649006f24
Vijay <vijaysinghkushwaha3737@gmail.com>
2023-09-24 18:07:56 +0530

    Github Login

---
+++ app/serverAction/page.tsx
@@ -0,0 +1,19 @@
+import { getServerSession } from "next-auth";
+import WhoAmIButton from "../components/WhoAmIButton";
+
+export default async function ServerActionPage() {
+    const whoAmI = async () => {
+        "use server"
+        const session = await getServerSession();
+        return session?.user?.name || "Not Logged In"
+    }
+
+    return (
+        <div>
+            <WhoAmIButton whoAmIAction={whoAmI} />
+        </div>
+    )
+    
+
+
+}
