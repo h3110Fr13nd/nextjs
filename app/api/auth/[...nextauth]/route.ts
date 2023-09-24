4a0ded17177669d3c5bd0b7689d0db3db13bb9a5
Vijay <vijaysinghkushwaha3737@gmail.com>
2023-09-24 18:43:00 +0530

    Disable type checking in typescript on build

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,16 +1,10 @@
 import NextAuth from "next-auth";
-import GithubProvider from "next-auth/providers/github";
+import { authOptions } from "./auth";
 
-export const authOptions = {
-    providers: [
-        GithubProvider({
-            clientId: process.env.GITHUB_ID ?? "",
-            clientSecret: process.env.GITHUB_SECRET ?? "",
-        }),
-    ],
-};
 
-export const handler = NextAuth(authOptions);
+
+
+export const handler: any = NextAuth(authOptions);
 
 export { handler as GET, handler as POST} ;
 
